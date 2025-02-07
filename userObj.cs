be7b307ead8bd8b66caf95e483b1454ae651e07a
jidapa.angsutti <jidapa.angsutti@studens.snow.edu>
2025-02-07 10:40:16 -0700

    Jidapa: can add task with a description ; changed User class and Task class

--- userObj.cs
+++ userObj.cs
@@ -1,17 +1,46 @@
 public class User
 {
-    public List<Task> tasks;
+    public string Name { get; set; }
+    public List<Task> Tasks = new();
+
+    public User(string name)
+    {
+        Name = name;
+    }
+
+    /// <summary>
+    /// Pass in title and description for task
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="description"></param>
+    public void AddTask(string title, string description)
+    {
+        Tasks.Add(new Task(title, description));
+    }
+
+    /// <summary>
+    /// Pass in the Task object
+    /// </summary>
+    /// <param name="task"></param>
+    public void AddTask(Task task)
+    {
+        Tasks.Add(task);
+    }
 }
 
 public class Task
 {
-    public Guid Id;
-    public string Title;
-    public string Description;
-    public bool Completed;
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool Completed { get; set; }
 
     public Task(string title, string description)
     {
+        Title = title;
+        Description = description;
 
+        Completed = false;
+        Id = Guid.NewGuid();
     }
 }
