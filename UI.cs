4ebf75eb9734c2d4cf0fa3f178778ffbabcf50ed
Landon Ginn <landon.ginn@students.snow.edu>
2025-02-07 11:08:15 -0700

    Menu Framework -Landon

--- UI.cs
+++ UI.cs
@@ -1,1 +1,77 @@
 ﻿//Landon
+
+var currentUser = new User();
+currentUser.tasks = new();
+
+
+Console.Clear();
+Console.WriteLine("Welcome to the HKJL To-Do List.");
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("To display your To-Do List and toggle completion, press '0'");
+Console.WriteLine("To add a new task, press '1'");
+Console.WriteLine("To delete tasks, press '2'");
+
+int selection = -1;
+while (selection == -1)
+{
+    try
+    {
+        selection = int.Parse(Console.ReadKey(true).KeyChar.ToString());
+    }
+    catch
+    {
+        Console.WriteLine(selection);
+    }
+    if (selection > 2 || selection < 0)
+    {
+        selection = -1;
+    }
+}
+
+switch (selection)
+{
+    case 0:
+        DisplayandToggleTasks();
+        break;
+
+    case 1:
+        AddTask();
+        break;
+
+    case 2:
+        DeleteTask();
+        break;
+
+}
+
+void DisplayandToggleTasks()
+{
+    Console.Clear();
+
+    if (currentUser.tasks.Count() == 0)
+    {
+        Console.WriteLine("You have no tasks on your To-Do List.");
+    }
+    else
+    {
+        foreach (Task task in currentUser.tasks)
+        {
+            if (task.completed)
+                Console.WriteLine($"- {task.title}: {task.description}  --  [COMPLETE]");
+            else
+                Console.WriteLine($"- {task.title}: {task.description}  --  [INCOMPLETE]");
+        }
+    }
+}
+
+void AddTask()
+{
+
+}
+
+void DeleteTask()
+{
+
+}
