3f02c832d22ed1490f0f50bb87feb3a3985d7bab
Kyler Olsen <kyler.olsen@students.snow.edu>
2025-02-07 11:06:35 -0700

    Updated variable names

--- toggleTask.cs
+++ toggleTask.cs
@@ -10,15 +10,15 @@ public class toggleTask {
 			Console.WriteLine("  0. Cancel");
 
 			int i = 1;
-			foreach (Task task in user.tasks) {
-				Console.WriteLine($"  {i}. {task.title}");
+			foreach (Task task in user.Tasks) {
+				Console.WriteLine($"  {i}. {task.Title}");
 				i++;
 			}
 			Console.Write("> ");
 
 			if (int.TryParse(Console.ReadLine(), out option)) {
 				if (option == 0) break;
-				else if (option < i) { toggle_task(user.tasks[option - 1]); break; }
+				else if (option < i) { toggle_task(user.Tasks[option - 1]); break; }
 				else Console.WriteLine("Please enter a valid number!");
 			}
 			else Console.WriteLine("Please enter a valid number!");
@@ -28,7 +28,7 @@ public class toggleTask {
 	public static void toggle_task(Task task) {
 		int option;
 
-		Console.WriteLine($"The task {task.title} is marked as" + (task.completed ? " " : " not ") + "completed.");
+		Console.WriteLine($"The task {task.Title} is marked as" + (task.Completed ? " " : " not ") + "completed.");
 
 		do {
 			Console.WriteLine("What would you like to mark it as?");
@@ -38,13 +38,13 @@ public class toggleTask {
 			Console.Write("> ");
 			if (int.TryParse(Console.ReadLine(), out option)) {
 				if (option == 0) break;
-				else if (option == 1) { task.completed = true; break; }
-				else if (option == 2) { task.completed = false; break; }
+				else if (option == 1) { task.Completed = true; break; }
+				else if (option == 2) { task.Completed = false; break; }
 				else Console.WriteLine("Please enter a valid number!");
 			}
 			else Console.WriteLine("Please enter a valid number!");
 		} while (true);
 
-		Console.WriteLine($"The task {task.title} is marked as" + (task.completed ? " " : " not ") + "completed.");
+		Console.WriteLine($"The task {task.Title} is marked as" + (task.Completed ? " " : " not ") + "completed.");
 	}
 }
